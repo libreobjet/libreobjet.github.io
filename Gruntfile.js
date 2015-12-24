a8a1f476c3e0a7ec9e6712483785d4fd66ebdac5
Julien Deswaef <juego@requiem4tv.com>
2015-12-24 12:08:37 -0500

    added tasks for jekyll

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,54 @@ module.exports = function(grunt) {
           'public/css/main.css': 'public/css/main.styl',
         }
       }
+    },
+    jekyll: {                             // Task
+      options: {                          // Universal options
+        src : './'
+      },
+      dist: {                             // Target
+        options: {                        // Target options
+          dest: '_site',
+          config: '_config.yml'
+        }
+      },
+      serve: {                            // Another target
+        options: {
+          serve: true,
+          dest: '_site',
+          drafts: true,
+          future: true
+        }
+      }
+    },
+    buildcontrol: {
+      options: {
+        dir: '_site',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:example_user/example_webapp.git',
+          branch: 'gh-pages'
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
+      }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+
   grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-build-control');
 
   // Default task(s).
-  grunt.registerTask('default', ['stylus']);
+  grunt.registerTask('default', ['stylus', 'jekyll:serve']);
 
 };
